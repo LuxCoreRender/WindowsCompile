2da9fb40e2e93fd6f18c481e821fd5ec1b5338a4
doug <doug@localhost>
2010-08-18 13:09:55 +0100

    Collect pylux binaries with install scripts

--- install-x86.bat
+++ install-x86.bat
@@ -12,6 +12,8 @@ IF NOT EXIST .\Dist\32 (
     echo.
     echo making Dist directory
     mkdir Dist\32
+    mkdir Dist\32\Python2
+    mkdir Dist\32\Python3
 )
 
 cd Dist\32
@@ -20,6 +22,8 @@ set INSTALL_PATH="%CD%"
 copy %BUILD_PATH%\Projects\Win32\LuxRender\luxrender.exe luxrender.exe
 copy %BUILD_PATH%\Projects\win32\Console\luxconsole.exe luxconsole.exe
 copy %BUILD_PATH%\Projects\Win32\LuxMerge\luxmerger.exe luxmerger.exe
+copy %BUILD_PATH%\Projects\Win32\Pylux2Release\python2\pylux.pyd Python2\
+copy %BUILD_PATH%\Projects\Win32\Pylux3Release\python3\pylux.pyd Python3\
 copy %LUX_X86_QT_ROOT%\bin\QtCore4.dll QtCore4.dll
 copy %LUX_X86_QT_ROOT%\bin\QtGui4.dll QtGui4.dll
 copy %LUX_X86_QT_ROOT%\bin\QtOpenGL4.dll QtOpenGL4.dll
