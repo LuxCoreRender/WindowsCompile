0095b60fafa780bac6aa5e0e1b9695bc75fa778b
Asbj?rn Heid <lordcrc@gmail.net>
2010-08-24 11:50:37 +0200

    Changed install scripts to output progress, based on a patch from helloworld922. Also made them copy VC DLL's for completeness.

--- install-x64.bat
+++ install-x64.bat
@@ -16,21 +16,36 @@ IF NOT EXIST .\Dist\64 (
 IF NOT EXIST .\Dist\64\Python2 (
     mkdir Dist\64\Python2
 )
-:: IF NOT EXIST .\Dist\64\Python3 (
-::    mkdir Dist\64\Python3
-:: )
+IF NOT "%BUILD_PYTHON3%" == "" (
+  IF NOT EXIST .\Dist\64\Python3 (
+    mkdir Dist\64\Python3
+  )
+)
 
 cd Dist\64
 set INSTALL_PATH="%CD%"
 
-copy %BUILD_PATH%\Projects\x64\LuxRender\luxrender.exe luxrender.exe
-copy %BUILD_PATH%\Projects\x64\Console\luxconsole.exe luxconsole.exe
-copy %BUILD_PATH%\Projects\x64\LuxMerge\luxmerger.exe luxmerger.exe
-copy %BUILD_PATH%\Projects\x64\Pylux2Release\python2\pylux.pyd Python2\
-:: copy %BUILD_PATH%\Projects\x64\Pylux3Release\python3\pylux.pyd Python3\
-copy %LUX_X64_QT_ROOT%\bin\QtCore4.dll QtCore4.dll
-copy %LUX_X64_QT_ROOT%\bin\QtGui4.dll QtGui4.dll
-:: copy %LUX_X64_QT_ROOT%\bin\QtOpenGL4.dll QtOpenGL4.dll
+echo Copying luxrender.exe
+copy "%BUILD_PATH%"\Projects\x64\LuxRender\luxrender.exe luxrender.exe
+echo Copying luxconsole.exe
+copy "%BUILD_PATH%"\Projects\x64\Console\luxconsole.exe luxconsole.exe
+echo Copying luxmerger.exe
+copy "%BUILD_PATH%"\Projects\x64\LuxMerge\luxmerger.exe luxmerger.exe
+echo Copying Python 2 pylux.pyd
+copy "%BUILD_PATH%"\Projects\x64\Pylux2Release\python2\pylux.pyd Python2\
+IF NOT "%BUILD_PYTHON3%" == "" (
+  echo Copying Python 3 pylux.pyd
+  copy "%BUILD_PATH%"\Projects\x64\Pylux3Release\python3\pylux.pyd Python3\
+)
+echo Copying Qt DLL's
+copy "%LUX_X64_QT_ROOT%"\bin\QtCore4.dll QtCore4.dll
+copy "%LUX_X64_QT_ROOT%"\bin\QtGui4.dll QtGui4.dll
+:: copy "%LUX_X64_QT_ROOT%"\bin\QtOpenGL4.dll QtOpenGL4.dll
+echo Copying Visual C++ CRT DLL's
+copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\Microsoft.VC90.CRT.manifest Microsoft.VC90.CRT.manifest
+copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\msvcm90.dll msvcm90.dll
+copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\msvcp90.dll msvcp90.dll
+copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\msvcr90.dll msvcr90.dll
 
 cd /d %BUILD_PATH%
 
--- install-x86.bat
+++ install-x86.bat
@@ -16,21 +16,36 @@ IF NOT EXIST .\Dist\32 (
 IF NOT EXIST .\Dist\32\Python2 (
     mkdir Dist\32\Python2
 )
-:: IF NOT EXIST .\Dist\32\Python3 (
-::    mkdir Dist\32\Python3
-:: )
+IF NOT "%BUILD_PYTHON3%" == "" (
+  IF NOT EXIST .\Dist\32\Python3 (
+    mkdir Dist\32\Python3
+  )
+)
 
 cd Dist\32
 set INSTALL_PATH="%CD%"
 
-copy %BUILD_PATH%\Projects\Win32\LuxRender\luxrender.exe luxrender.exe
-copy %BUILD_PATH%\Projects\win32\Console\luxconsole.exe luxconsole.exe
-copy %BUILD_PATH%\Projects\Win32\LuxMerge\luxmerger.exe luxmerger.exe
-copy %BUILD_PATH%\Projects\Win32\Pylux2Release\python2\pylux.pyd Python2\
-:: copy %BUILD_PATH%\Projects\Win32\Pylux3Release\python3\pylux.pyd Python3\
-copy %LUX_X86_QT_ROOT%\bin\QtCore4.dll QtCore4.dll
-copy %LUX_X86_QT_ROOT%\bin\QtGui4.dll QtGui4.dll
-:: copy %LUX_X86_QT_ROOT%\bin\QtOpenGL4.dll QtOpenGL4.dll
+echo Copying luxrender.exe
+copy "%BUILD_PATH%"\Projects\Win32\LuxRender\luxrender.exe luxrender.exe
+echo Copying luxconsole.exe
+copy "%BUILD_PATH%"\Projects\Win32\Console\luxconsole.exe luxconsole.exe
+echo Copying luxmerger.exe
+copy "%BUILD_PATH%"\Projects\Win32\LuxMerge\luxmerger.exe luxmerger.exe
+echo Copying Python 2 pylux.pyd
+copy "%BUILD_PATH%"\Projects\Win32\Pylux2Release\python2\pylux.pyd Python2\
+IF NOT "%BUILD_PYTHON3%" == "" (
+  echo Copying Python 3 pylux.pyd
+  copy "%BUILD_PATH%"\Projects\Win32\Pylux3Release\python3\pylux.pyd Python3\
+)
+echo Copying Qt DLL's
+copy "%LUX_X86_QT_ROOT%"\bin\QtCore4.dll QtCore4.dll
+copy "%LUX_X86_QT_ROOT%"\bin\QtGui4.dll QtGui4.dll
+:: copy "%LUX_X86_QT_ROOT%\bin\QtOpenGL4.dll" QtOpenGL4.dll
+echo Copying Visual C++ CRT DLL's
+copy "%VCINSTALLDIR%"\redist\x86\Microsoft.VC90.CRT\Microsoft.VC90.CRT.manifest Microsoft.VC90.CRT.manifest
+copy "%VCINSTALLDIR%"\redist\x86\Microsoft.VC90.CRT\msvcm90.dll msvcm90.dll
+copy "%VCINSTALLDIR%"\redist\x86\Microsoft.VC90.CRT\msvcp90.dll msvcp90.dll
+copy "%VCINSTALLDIR%"\redist\x86\Microsoft.VC90.CRT\msvcr90.dll msvcr90.dll
 
 cd /d %BUILD_PATH%
 
