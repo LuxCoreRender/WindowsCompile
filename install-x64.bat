fd65934846524afb813446e31aefa934ef78658b
Doug Hammond <Doug Hammond@localhost>
2010-09-08 22:08:17 +0100

    Massive improvements to install scripts.

--- install-x64.bat
+++ install-x64.bat
@@ -5,50 +5,50 @@ echo ***************************************************************************
 echo * Install
 echo ****************************************************************************
 
-cd /d %BUILD_PATH%
+cd /d %LUX_WINDOWS_BUILD_ROOT%
 cd ..
 
-IF NOT EXIST .\Dist\64 (
-    echo.
-    echo making Dist\64 directory
-    mkdir Dist\64
+set INSTALL_PATH="%CD%"\Dist\64
+
+IF NOT EXIST %INSTALL_PATH% (
+	echo.
+	echo making %INSTALL_PATH% directory
+	mkdir %INSTALL_PATH%
 )
-IF NOT EXIST .\Dist\64\Python2 (
-    mkdir Dist\64\Python2
+IF NOT EXIST %INSTALL_PATH%\Python2 (
+	mkdir %INSTALL_PATH%\Python2
 )
-IF NOT "%LUX_BUILD_PYTHON3%" == "" (
-  IF NOT EXIST .\Dist\64\Python3 (
-    mkdir Dist\64\Python3
-  )
+IF NOT EXIST %INSTALL_PATH%\Python3 (
+	mkdir %INSTALL_PATH%\Python3
 )
 
-cd Dist\64
-set INSTALL_PATH="%CD%"
+cd %INSTALL_PATH%
 
 echo Copying luxrender.exe
-copy "%BUILD_PATH%"\Projects\luxrender\x64\LuxRender\luxrender.exe luxrender.exe
+copy "%LUX_WINDOWS_BUILD_ROOT%"\Projects\luxrender\x64\Release\luxrender.exe .\ > nul
 echo Copying luxconsole.exe
-copy "%BUILD_PATH%"\Projects\luxrender\x64\Console\luxconsole.exe luxconsole.exe
+copy "%LUX_WINDOWS_BUILD_ROOT%"\Projects\luxrender\x64\Release\luxconsole.exe .\ > nul
 echo Copying luxmerger.exe
-copy "%BUILD_PATH%"\Projects\luxrender\x64\LuxMerge\luxmerger.exe luxmerger.exe
+copy "%LUX_WINDOWS_BUILD_ROOT%"\Projects\luxrender\x64\Release\luxmerger.exe .\ > nul
 echo Copying Python 2 pylux.pyd
-copy "%BUILD_PATH%"\Projects\luxrender\x64\Pylux2Release\python2\pylux.pyd Python2\
-IF NOT "%LUX_BUILD_PYTHON3%" == "" (
-  echo Copying Python 3 pylux.pyd
-  copy "%BUILD_PATH%"\Projects\luxrender\x64\Pylux3Release\python3\pylux.pyd Python3\
-)
-echo Copying Qt DLL's
-copy "%LUX_X64_QT_ROOT%"\bin\QtCore4.dll QtCore4.dll
-copy "%LUX_X64_QT_ROOT%"\bin\QtGui4.dll QtGui4.dll
-:: copy "%LUX_X64_QT_ROOT%"\bin\QtOpenGL4.dll QtOpenGL4.dll
-echo Copying Visual C++ CRT DLL's
-copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\Microsoft.VC90.CRT.manifest Microsoft.VC90.CRT.manifest
-copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\msvcm90.dll msvcm90.dll
-copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\msvcp90.dll msvcp90.dll
-copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\msvcr90.dll msvcr90.dll
+copy "%LUX_WINDOWS_BUILD_ROOT%"\Projects\luxrender\x64\Release\python2\pylux.pyd Python2\ > nul
+echo Copying Python 3 pylux.pyd
+copy "%LUX_WINDOWS_BUILD_ROOT%"\Projects\luxrender\x64\Release\python3\pylux.pyd Python3\ > nul
 
-cd /d %BUILD_PATH%
+echo Copying Qt DLL's
+copy "%LUX_X64_QT_ROOT%"\bin\QtCore4.dll . > nul
+copy "%LUX_X64_QT_ROOT%"\bin\QtGui4.dll . > nul
+:: copy "%LUX_X64_QT_ROOT%"\bin\QtOpenGL4.dll . > nul
+
+echo Copying Visual C++ CRT DLL's
+copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\Microsoft.VC90.CRT.manifest . > nul
+copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\msvcm90.dll . > nul
+copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\msvcp90.dll . > nul
+copy "%VCINSTALLDIR%"\redist\amd64\Microsoft.VC90.CRT\msvcr90.dll . > nul
+
+cd /d %LUX_WINDOWS_BUILD_ROOT%
 
 echo.
-echo 64 bit binaries are availabe for use in %INSTALL_PATH%
+echo 64 bit binaries are available for use in
+echo     %INSTALL_PATH%
 echo.
