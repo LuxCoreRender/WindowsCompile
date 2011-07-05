1fb88685c01c205d7a6169439f00421290066d4c
Asbj?rn Heid <lordcrc@gmail.net>
2011-07-05 02:58:20 +0200

    Update getdeps for new AMD SDK, fix bug in cl.hpp.

--- include/CL/cl.hpp
+++ include/CL/cl.hpp
@@ -2968,7 +2968,7 @@ public:
     }
 
     cl_int enqueueNativeKernel(
-        void (*userFptr)(void *),
+        void (CL_CALLBACK *userFptr)(void *),
         std::pair<void*, ::size_t> args,
         const VECTOR_CLASS<Memory>* mem_objects = NULL,
         const VECTOR_CLASS<const void*>* mem_locs = NULL,
