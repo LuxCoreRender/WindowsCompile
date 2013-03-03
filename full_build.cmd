43d9b2c80289428932ccab5a87205abd50f665f4
Asbj?rn Heid <lordcrc@gmail.net>
2013-03-04 00:56:57 +0100

    VS2010: Updated full_build.cmd

--- full_build.cmd
+++ full_build.cmd
@@ -1,7 +1,5 @@
 set LUX_WINDOWS_BUILD_ROOT=%CD%
 
-goto luxrays
-
 msbuild /v:m /m full_build.txt
 
 if NOT ERRORLEVEL 1 goto :install
@@ -18,20 +16,18 @@ cd /D %LUX_WINDOWS_BUILD_ROOT%
 call install-x64.bat
 call install-x86.bat
 
-:luxrays
 
 del changelog.old.txt >nul
 ren changelog.txt changelog.old.txt >nul
-for /f %%a in (last_changeset.txt) do hg shortlog --branch slg_renderer -r "not branch(default)" --prune %%a -R ..\lux\ >> changelog.txt
+for /f %%a in (last_changeset.txt) do hg shortlog --prune %%a -R ..\lux\ >> changelog.txt
 
 move /y last_changeset.txt old_changeset.txt >nul
 for /f "tokens=1,2" %%a in ('hg sum -R ..\lux') do if "%%a"=="parent:" (for /f "delims=: tokens=2" %%c in ("%%b") do echo %%c > last_changeset.txt)
 
 
-
 del changelog.luxrays.old.txt >nul
 ren changelog.luxrays.txt changelog.luxrays.old.txt >nul
-for /f %%a in (last_changeset.luxrays.txt) do hg shortlog --branch slg3.5_startup -r "not branch(default)" --prune %%a -R ..\luxrays\ >> changelog.luxrays.txt
+for /f %%a in (last_changeset.luxrays.txt) do hg shortlog --prune %%a -R ..\luxrays\ >> changelog.luxrays.txt
 
 move /y last_changeset.luxrays.txt old_changeset.luxrays.txt >nul
 for /f "tokens=1,2" %%a in ('hg sum -R ..\luxrays') do if "%%a"=="parent:" (for /f "delims=: tokens=2" %%c in ("%%b") do echo %%c > last_changeset.luxrays.txt)
