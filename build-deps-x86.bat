903418b641fd46708c7de421f86e6ceb73a68df5
Omni Flux <omniflux@omniflux.com>
2014-03-17 02:54:04 -0600

    Support building 32bit 3rd party dependencies

--- build-deps-x86.bat
+++ build-deps-x86.bat
@@ -88,7 +88,7 @@ GOTO DebugChoice
 IF %BUILD_DEBUG% EQU 0 set BUILD_CONFIGURATION=Release
 IF %BUILD_DEBUG% EQU 1 set BUILD_CONFIGURATION=Debug
 
-set INSTALL_DIR=%LUX_WINDOWS_DEPS_ROOT%\x64\%BUILD_CONFIGURATION%
+set INSTALL_DIR=%LUX_WINDOWS_DEPS_ROOT%\x86\%BUILD_CONFIGURATION%
 set CMAKE_OPTS=-G "Visual Studio 12" -D CMAKE_PREFIX_PATH="%INSTALL_DIR%" -D BUILD_SHARED_LIBS=0 -D BOOST_ROOT="%LUX_X86_BOOST_ROOT%" -D Boost_USE_STATIC_LIBS=1 -D QT_QMAKE_EXECUTABLE="%LUX_X86_QT_ROOT%\bin\qmake"
 
 set MSBUILD_OPTS=/nologo /maxcpucount /verbosity:quiet /toolsversion:12.0 /property:"PlatformToolset=v120" /property:"Platform=Win32" /property:ForceImportBeforeCppTargets=%LUX_WINDOWS_BUILD_ROOT%\Support\MultiThreadedDLL.props /target:"Clean"
@@ -144,8 +144,8 @@ pause
 set BUILD_CONFIGURATION_QT=-release -ltcg
 IF %BUILD_CONFIGURATION%==Debug set BUILD_CONFIGURATION_QT=-debug
 
-configure -opensource -fast -mp -nomake demos -nomake examples -no-multimedia -no-phonon -no-phonon-backend -no-audio-backend -no-webkit -no-script -no-scripttools -no-qt3support %BUILD_CONFIGURATION_QT%
-nmake
+cmd /S /C ""%VCINSTALLDIR%\vcvarsall" x86 && configure -opensource -fast -mp -nomake demos -nomake examples -no-multimedia -no-phonon -no-phonon-backend -no-audio-backend -no-webkit -no-script -no-scripttools -no-qt3support %BUILD_CONFIGURATION_QT%"
+cmd /S /C ""%VCINSTALLDIR%\vcvarsall" x86 && nmake"
 
 
 :NotQT
@@ -167,10 +167,10 @@ msbuild %MSBUILD_OPTS% /property:"Configuration=%BUILD_CONFIGURATION%" /target:"
 
 mkdir %INCLUDE_DIR%\Python3.3
 copy ..\include\*.h %INCLUDE_DIR%\Python3.3
-IF %BUILD_CONFIGURATION%==Release copy amd64\python33.lib %LIB_DIR%
-IF %BUILD_CONFIGURATION%==Release copy amd64\python33.dll %LIB_DIR%
-IF %BUILD_CONFIGURATION%==Debug   copy amd64\python33_d.lib %LIB_DIR%
-IF %BUILD_CONFIGURATION%==Debug   copy amd64\python33_d.dll %LIB_DIR%
+IF %BUILD_CONFIGURATION%==Release copy python33.lib %LIB_DIR%
+IF %BUILD_CONFIGURATION%==Release copy python33.dll %LIB_DIR%
+IF %BUILD_CONFIGURATION%==Debug   copy python33_d.lib %LIB_DIR%
+IF %BUILD_CONFIGURATION%==Debug   copy python33_d.dll %LIB_DIR%
 
 :: ****************************************************************************
 :: ******************************* BOOST **************************************
@@ -195,7 +195,7 @@ rem Patch Boost 1.55.0 to build Serialization with Visual Studio 2013
 %LUX_WINDOWS_BUILD_ROOT%\support\bin\patch --forward --backup --batch -p1 -i %LUX_WINDOWS_BUILD_ROOT%\support\boost_1.55.0.patch
 
 CALL bootstrap.bat
-type %LUX_WINDOWS_BUILD_ROOT%\support\x64-project-config-3.jam >> project-config.jam
+type %LUX_WINDOWS_BUILD_ROOT%\support\x86-project-config-3.jam >> project-config.jam
 set BJAM_OPTS=-a -q -j%NUMBER_OF_PROCESSORS% link=static threading=multi runtime-link=shared --with-date_time --with-filesystem --with-iostreams --with-locale --with-program_options --with-python --with-regex --with-serialization --with-system --with-thread -sBZIP2_SOURCE=%LUX_X86_BZIP_ROOT% -sPYTHON_SOURCE=%LUX_X86_PYTHON3_ROOT% -sZLIB_SOURCE=%LUX_X86_ZLIB_ROOT%
 
 set BUILD_CONFIGURATION_BOOST=release
@@ -218,7 +218,7 @@ msbuild %MSBUILD_OPTS% /property:"Configuration=%BUILD_CONFIGURATION%_Static" /t
 
 mkdir %INCLUDE_DIR%\GL
 copy include\GL\*.h %INCLUDE_DIR%\GL
-copy lib\x64\* %LIB_DIR%
+copy lib\x86\* %LIB_DIR%
 
 
 :: ****************************************************************************
@@ -234,7 +234,7 @@ cd /d %LUX_X86_FFTW_ROOT%
 msbuild %MSBUILD_OPTS% /property:"Configuration=Static-%BUILD_CONFIGURATION%" /target:"libfftw-3_3" fftw-3.3-libs\fftw-3.3-libs.sln
 
 copy api\fftw3.h %INCLUDE_DIR%
-copy fftw-3.3-libs\x64\Static-%BUILD_CONFIGURATION%\*.lib %LIB_DIR%
+copy fftw-3.3-libs\Static-%BUILD_CONFIGURATION%\*.lib %LIB_DIR%
 
 
 :: ****************************************************************************
@@ -254,8 +254,8 @@ msbuild %MSBUILD_OPTS% /property:"Configuration=%BUILD_CONFIGURATION%" /target:"
 
 mkdir %INCLUDE_DIR%\GL
 copy include\GL\*.h %INCLUDE_DIR%\GL
-IF %BUILD_CONFIGURATION%==Release copy lib\Release\x64\glew32s.lib %LIB_DIR%\glew32.lib
-IF %BUILD_CONFIGURATION%==Debug   copy lib\Debug\x64\glew32sd.lib %LIB_DIR%\glew32.lib
+IF %BUILD_CONFIGURATION%==Release copy lib\Release\Win32\glew32s.lib %LIB_DIR%\glew32.lib
+IF %BUILD_CONFIGURATION%==Debug   copy lib\Debug\Win32\glew32sd.lib %LIB_DIR%\glew32.lib
 
 
 :: ****************************************************************************
@@ -275,7 +275,7 @@ copy jconfig.vc jconfig.h
 msbuild %MSBUILD_OPTS% /property:"Configuration=%BUILD_CONFIGURATION%" /target:"jpeg" jpeg.sln
 
 copy *.h %INCLUDE_DIR%
-copy x64\%BUILD_CONFIGURATION%\*.lib %LIB_DIR%
+copy %BUILD_CONFIGURATION%\*.lib %LIB_DIR%
 
 
 :: ****************************************************************************
@@ -371,12 +371,12 @@ echo **************************************************************************
 cd /d %LUX_X86_LIBTIFF_ROOT%
 
 rem Update project files
-%LUX_WINDOWS_BUILD_ROOT%\support\bin\patch --forward --backup --batch nmake.opt %LUX_WINDOWS_BUILD_ROOT%\support\libtiff.nmake.opt.x64.patch
+%LUX_WINDOWS_BUILD_ROOT%\support\bin\patch --forward --backup --batch nmake.opt %LUX_WINDOWS_BUILD_ROOT%\support\libtiff.nmake.opt.x86.patch
 
 nmake /f Makefile.vc Clean
 
-IF %BUILD_CONFIGURATION%==Release nmake /f Makefile.vc 
-IF %BUILD_CONFIGURATION%==Debug nmake /f Makefile.vc DEBUG=1
+IF %BUILD_CONFIGURATION%==Release cmd /S /C ""%VCINSTALLDIR%\vcvarsall" x86 && nmake /f Makefile.vc"
+IF %BUILD_CONFIGURATION%==Debug cmd /S /C ""%VCINSTALLDIR%\vcvarsall" x86 && nmake /f Makefile.vc DEBUG=1"
 
 copy libtiff\*.h %INCLUDE_DIR%
 copy libtiff\libtiff.lib %LIB_DIR%
