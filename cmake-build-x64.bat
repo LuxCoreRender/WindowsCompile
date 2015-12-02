d5c08fa65e891a7b5baafca9fb080ba167847a30
David Bucciarelli <dade916@gmail.com>
2015-12-02 11:24:08 +0100

    Added the support for Debug compilation

--- cmake-build-x64.bat
+++ cmake-build-x64.bat
@@ -9,6 +9,7 @@ set BUILD_LUXRENDER_ONLY=0
 set MSBUILD_PLATFORM=x64
 set DISABLE_OPENCL=0
 set CPU_PLATFORM=x64
+set BUILD_TYPE=Release
 
 :ParseCmdParams
 if "%1" EQU "" goto Start
@@ -17,6 +18,7 @@ if /i "%1" EQU "luxrays" set BUILD_LUXRAYS_ONLY=1
 if /i "%1" EQU "luxmark" set BUILD_LUXMARK_ONLY=1
 if /i "%1" EQU "luxrender" set BUILD_LUXRENDER_ONLY=1
 if /i "%1" EQU "/no-ocl" set DISABLE_OPENCL=1
+if /i "%1" EQU "/debug" set BUILD_TYPE=Debug
 if /i "%1" EQU "/x86" (
   set CPU_PLATFORM=x86
   set MSBUILD_PLATFORM=Win32
@@ -96,8 +98,9 @@ if %DISABLE_OPENCL% EQU 1 (
   )
 )
 
-set CMAKE_OPTS=-G %CMAKE_GENERATOR% %CMAKE_PLATFORM% %CMAKE_TOOLSET% -D CMAKE_INCLUDE_PATH="%INCLUDE_DIR%" -D CMAKE_LIBRARY_PATH="%LIB_DIR%" -D PYTHON_LIBRARY="%LIB_DIR%" -D PYTHON_INCLUDE_DIR="%INCLUDE_DIR%\Python3" -D CMAKE_BUILD_TYPE=RELEASE %OCL_OPTION%
-set MSBUILD_OPTS=/nologo /maxcpucount /verbosity:normal /toolsversion:12.0  /property:"Platform=%MSBUILD_PLATFORM%" /property:"Configuration=Release" /clp:ErrorsOnly
+set CMAKE_OPTS=-G %CMAKE_GENERATOR% %CMAKE_PLATFORM% %CMAKE_TOOLSET% -D CMAKE_INCLUDE_PATH="%INCLUDE_DIR%" -D CMAKE_LIBRARY_PATH="%LIB_DIR%" -D PYTHON_LIBRARY="%LIB_DIR%" -D PYTHON_INCLUDE_DIR="%INCLUDE_DIR%\Python3" -D CMAKE_BUILD_TYPE=%BUILD_TYPE% %OCL_OPTION%
+rem To display only errors add: /clp:ErrorsOnly
+set MSBUILD_OPTS=/nologo /maxcpucount /verbosity:normal /toolsversion:12.0 /property:"Platform=%MSBUILD_PLATFORM%" /property:"Configuration=%BUILD_TYPE%" /p:WarningLevel=0
 
 if %FULL_REBUILD%==1 rd /q /s Build_CMake
 mkdir Build_CMake
