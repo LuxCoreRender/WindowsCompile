b484049d1f0463aa97308fb9fb781cbff2fe4fde
Theverat <simonwendsche@gmx.de>
2017-12-07 17:55:18 +0100

    added batch script to collect the compiled binaries

---
+++ collect_compiled_binaries.bat
@@ -0,0 +1,15 @@
+SET DIR=compiled_binaries
+
+:: Remove folder if it already exists
+rd /s /q %DIR%
+
+:: Create new folder for the binaries
+md %DIR%
+
+:: Copy binaries
+xcopy .\Build_CMake\LuxCore\bin\Release\luxcoreui.exe %DIR%
+xcopy .\Build_CMake\LuxCore\lib\Release\pyluxcore.pyd %DIR%
+
+:: Copy DLLs from WindowsCompileDeps (assuming it is in same folder as WindowsCompile)
+xcopy ..\WindowsCompileDeps\x64\Release\lib\embree.dll %DIR%
+xcopy ..\WindowsCompileDeps\x64\Release\lib\tbb.dll %DIR%
