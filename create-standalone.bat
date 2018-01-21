4b35bf98d4313548ebecca65f5756d2bced411aa
David Bucciarelli <dade916@gmail.com>
2018-01-21 13:56:02 +0100

    Added the support for pyinstaller

--- create-standalone.bat
+++ create-standalone.bat
@@ -6,9 +6,27 @@ rd /s /q %DIR%
 :: Create new folder for the binaries
 md %DIR%
 
+:: Create pyluxcoretools.zip (cmake tar doesn't work on Windows)
+"C:\Program Files\7-Zip\7z.exe" a ..\LuxCore\lib\pyluxcoretools.zip ..\LuxCore\src\pyluxcoretools\pyluxcoretools
+
+:: Pack pyluxcoretools
+cd ..\LuxCore
+pyinstaller samples\pyluxcoreconsole\pyluxcoreconsole.win.spec
+pyinstaller samples\pyluxcoremerge\pyluxcoremerge.win.spec
+pyinstaller samples\pyluxcorenetconsole\pyluxcorenetconsole.win.spec
+pyinstaller samples\pyluxcorenetnode\pyluxcorenetnode.win.spec
+cd ..\WindowsCompile
+
+:: Copy pyluxcoretools binaries
+xcopy ..\LuxCore\dist\pyluxcoreconsole.exe %DIR%
+xcopy ..\LuxCore\dist\pyluxcoremerge.exe %DIR%
+xcopy ..\LuxCore\dist\pyluxcorenetconsole.exe %DIR%
+xcopy ..\LuxCore\dist\pyluxcorenetnode.exe %DIR%
+
 :: Copy binaries
 xcopy .\Build_CMake\LuxCore\bin\Release\luxcoreui.exe %DIR%
 xcopy .\Build_CMake\LuxCore\lib\Release\pyluxcore.pyd %DIR%
+xcopy ..\LuxCore\lib\pyluxcoretools.zip %DIR%
 
 :: Copy DLLs from WindowsCompileDeps (assuming it is in same folder as WindowsCompile)
 xcopy ..\WindowsCompileDeps\x64\Release\lib\embree.dll %DIR%
