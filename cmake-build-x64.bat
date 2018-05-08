6b6ec7fda98900451fac6374f579081f143c3319
David Bucciarelli <dade916@gmail.com>
2018-05-08 10:15:12 +0200

    Fixed a problem with spaces in cmake variable

--- cmake-build-x64.bat
+++ cmake-build-x64.bat
@@ -50,7 +50,7 @@ if not exist "%CMAKE%" goto CMakeNotFound
 if not exist "%LUXCORE_ROOT%" goto LuxCoreNotFound
 
 :: Determine if we have CMake 2 or 3
-for /F "tokens=3" %%G in ('%CMAKE% --version ^| find "cmake version"') do set CMAKE_VER=%%G
+for /F "tokens=3" %%G in ('cmd /c "%CMAKE%" --version ^| find "cmake version"') do set CMAKE_VER=%%G
 for /F "tokens=1 delims=." %%G in ("%CMAKE_VER%") do set CMAKE_VN_MAJOR=%%G
 echo We are using CMake version: %CMAKE_VN_MAJOR%
 :: Default values
