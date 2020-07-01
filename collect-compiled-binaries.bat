c2353a0d689f35cd2e24c31036593d1062d724be
Simon Wendsche <simon.wendsche@gmx.de>
2020-07-01 17:12:06 +0200

    Update Intel Oidn to v1.2.1

--- collect-compiled-binaries.bat
+++ collect-compiled-binaries.bat
@@ -12,7 +12,7 @@ xcopy .\Build_CMake\LuxCore\lib\Release\pyluxcore.pyd %DIR%
 
 :: Copy DLLs from WindowsCompileDeps (assuming it is in same folder as WindowsCompile)
 xcopy ..\WindowsCompileDeps\x64\Release\lib\OpenImageDenoise.dll %DIR%
-xcopy ..\WindowsCompileDeps\x64\Release\lib\denoise.exe %DIR%
+xcopy ..\WindowsCompileDeps\x64\Release\lib\oidnDenoise.exe %DIR%
 xcopy ..\WindowsCompileDeps\x64\Release\lib\embree3.dll %DIR%
 xcopy ..\WindowsCompileDeps\x64\Release\lib\tbb.dll %DIR%
 xcopy ..\WindowsCompileDeps\x64\Release\lib\tbbmalloc.dll %DIR%
